ecda76c310a9545674c0409929209839a6cc3ba3
Dan Truong <dodantruong333@gmail.com>
2024-08-20 21:06:35 +0700

    Create funcs for basic op, a func for multiple op
    funcs for basic op is: Addition, Subtraction, Multiplication, Divsion.
    a func that take 2 number and a mathematical expession to decide which kind of operation to perform.

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-let num1, num2
-let expression
+let number1, number2, result
+let mathematicalExpression
+
+//4 fucntions for basic mathematical operation, a func take 2 number and a mathematical expression
+function doAddition(num1, num2) {
+    return num1 + num2
+}
+
+function doSubtraction(num1, num2) {
+    return num1 - num2
+}
+
+function doMultiplication(num1, num2) {
+    return num1 * num2
+}
+
+function doDivision(num1, num2) {
+    return num1 / num2
+}
+
+function doMath(num1, num2, expression) {
+    switch(expression) {
+        case '+': result = doAddition(num1, num2)
+        case '-': result = doSubtraction(num1, num2)
+        case '*': result = doMultiplication(num1, num2)
+        case '/': result = doDivision(num1, num2)
+    }
+}
 
