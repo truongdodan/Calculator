15b0aedba5231629e7d3ba88593d7d65a24d368a
Dan Truong <dodantruong333@gmail.com>
2024-08-26 20:24:38 +0700

    Make the numbers and expressions be display

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ function doMath(num1, num2, expression) {
     }
 }
 
+//Show the charactor on the monitor
+const btns = document.querySelectorAll("button")
+const display = document.querySelector("#display")
+const sp = document.querySelector("#space")
+const cl = document.querySelector("#clear")
+
+// btn.addEventListener("click", function () {
+//     display.textContent = btn.textContent;
+// })
+
+sp.addEventListener("click", function() {
+    let text = display.textContent
+    display.textContent = text.slice(0, -1)
+})
+
+cl.addEventListener("click", function() {
+    display.textContent = ''
+})
+
+btns.forEach(btn => {
+    if(btn.id == "space" || btn.id == "clear" || btn.id == "equal") {}
+    else {
+        btn.addEventListener("click", function() {
+            display.textContent += btn.textContent;
+        })
+    }
+})
+
+
+
+
+
+
